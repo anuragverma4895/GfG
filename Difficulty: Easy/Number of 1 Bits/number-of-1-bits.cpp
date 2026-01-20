e5ccb2a253a6f740edf9c1936cdaff9eb13bb08c
Anurag Verma <anuragverma4895@gmail.com>
2026-01-20 14:25:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of 1 Bits/number-of-1-bits.cpp
+++ Difficulty: Easy/Number of 1 Bits/number-of-1-bits.cpp
@@ -1,8 +1,19 @@
 class Solution {
   public:
-    int setBits(int n) {
-        string s=bitset<32>(n).to_string();
-        int c=count(s.begin(),s.end(),'1');
-        return c;
+    // solve using function
+    // int setBits(int n) {
+    //     string s=bitset<32>(n).to_string();
+    //     int c=count(s.begin(),s.end(),'1');
+    //     return c;
+    // }
+    
+    // using bit manupulation
+    int setBits(int n){
+        int count=0;
+        while(n){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
     }
 };
